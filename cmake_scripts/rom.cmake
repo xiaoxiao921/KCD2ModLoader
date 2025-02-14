1b7655eb27e8a9b111d0cc2302e8570a2593c687
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-02-14 18:53:57 +0100

    use custom lua repo (hosting the cryengine fork)

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -1,10 +1,10 @@
 include(FetchContent)
 
+set(LUA_CUSTOM_REPO https://github.com/xiaoxiao921/lua-fork.git)
+
+set(LUA_GIT_HASH 1d99acdb3a1bd69aadaa0d8c1b00de80e283dafa)
+
 set(LUA_USE_LUAJIT false)
-
-set(LUA_GIT_HASH 98194db4295726069137d13b8d24fca8cbf892b6)
-
-set(LUA_PATCH_PATH ${CMAKE_CURRENT_SOURCE_DIR}/patches/lua.patch)
 
 add_compile_definitions(
     "IMGUI_USER_CONFIG=\"${SRC_DIR}/gui/imgui_config.hpp\""
@@ -13,6 +13,6 @@ add_compile_definitions(
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG c8b792a6a5bee2069b3cbb30db5d06c5176bfcca
+	GIT_TAG b5fe9fbc0285a27531034191d43e59ba9c8d7d5d
 )
 FetchContent_MakeAvailable(rom)
