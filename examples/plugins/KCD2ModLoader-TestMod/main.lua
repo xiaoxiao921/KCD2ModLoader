d2f2d2978b2bfacbd32287cdb453cb785b426f58
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-02-18 23:28:19 +0100

    update example

--- examples/plugins/KCD2ModLoader-TestMod/main.lua
+++ examples/plugins/KCD2ModLoader-TestMod/main.lua
@@ -1,10 +1,21 @@
-local myVariableYep = 0
-
-for i = 1, 3 do
-	myVariableYep = myVariableYep + i
+-- early_main is required for some data modifcations, 
+-- like when fmod audio getevent is parsed at game start for sound replacement,
+-- or when some xml files are parsed by the game.
+-- Generally the lua documentation files will tell you if some game events require a is_early_main check
+-- for them to do anything meaningful.
+if rom.is_early_main then
+	rom.log.warning("hello from early")
+	
+	rom.audio.on_fmod_getevent(function(event_name)
+		rom.log.info(event_name)
+	end)
+	
+	rom.xml.on_xml_parse(function(filename, file_content)
+		rom.log.info(filename)
+	end)
+	
+	return
 end
-
-rom.log.warning("hello world", tostring(18), _VERSION, myVariableYep)
 
 local example_bool = false
 rom.gui.add_to_menu_bar(function()
@@ -12,15 +23,26 @@ rom.gui.add_to_menu_bar(function()
     if clicked then
         example_bool = new_value
 		rom.log.info(example_bool)
-    end
+	end
 end)
 
-
-for k,v in pairs(_G) do
+-- for k,v in pairs(_G) do
 	-- rom.log.warning(k,v)
-end
+-- end
+
+-- for k,v in pairs(rom.audio.events) do
+	-- rom.log.warning(k,v)
+-- end
+
+-- rom.log.set_output_vanilla_game_log(false)
+
+rom.xml.on_xml_parse(function(filename, file_content)
+	rom.log.info(filename)
+end)
 
 rom.gui.add_imgui(function()
+    -- rom.ImGui.PushStyleColor(rom.ImGuiCol.WindowBg, 1, 1, 1, 1)
+    
    if rom.ImGui.Begin("My Custom Window") then
        if rom.ImGui.Button("Label") then
          rom.log.info("hi")
@@ -28,4 +50,6 @@ rom.gui.add_imgui(function()
 	
    end
    rom.ImGui.End()
+   
+   -- rom.ImGui.PopStyleColor()
 end)
